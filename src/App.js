3549b4f2a2c4b0367f016291ad365f7e628470a1
AniketDaware <aniketdaware1912@gmail.com>
2023-06-28 13:09:34 +0530

    App.js updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [images, setImages] = useState([]);
+
+  const handleSearch = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.unsplash.com/search/photos?query=${searchTerm}&client_id=YOUR_UNSPLASH_ACCESS_KEY`
+      );
+      setImages(response.data.results);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Image Search</h1>
+      <input
+        type="text"
+        placeholder="Enter search text"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <button onClick={handleSearch}>Search</button>
+
+      <div className="imageGrid">
+        {images.map((image) => (
+          <div className="imageItem" key={image.id}>
+            <img src={image.urls.small} alt={image.alt_description} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
